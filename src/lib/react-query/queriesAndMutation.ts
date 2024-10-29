7600c9cc804a482deef68062146664b8bab91a27
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2024-10-29 22:24:29 +0600

    third commit

--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -1,9 +1,4 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  useInfiniteQuery,
-} from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/lib/react-query/queryKeys.ts";
 import {
   createPost,
@@ -11,7 +6,6 @@ import {
   deletePost,
   deleteSavedPost,
   getCurrentUser,
-  getInfinitePosts,
   getPostById,
   getRecentPosts,
   getUserById,
@@ -171,19 +165,22 @@ export const useDeletePost = () => {
   });
 };
 
-export const useGetPosts = () => {
-  return useInfiniteQuery({
-    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
-      if (lastPage && lastPage.documents.length === 0) return null;
+// export const useGetPosts = () => {
+//   return useInfiniteQuery({
+//     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+//     queryFn: getInfinitePosts,
+//     getNextPageParam: (lastPage) => {
+//       // If there's no data, there are no more pages.
+//       if (lastPage && lastPage.documents.length === 0) {
+//         return null;
+//       }
 
-      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
-
-      return lastId;
-    },
-  });
-};
+//       // Use the $id of the last document as the cursor.
+//       const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
+//       return lastId;
+//     },
+//   });
+// };
 
 export const useSearchPosts = (searchTerm: string) => {
   return useQuery({
